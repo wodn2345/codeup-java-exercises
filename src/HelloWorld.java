ce8230967404adcbf5623330281b306fd37f3cd9
Jaewoo Baek <jaewoo.baek01@gmail.com>
2023-03-24 14:29:03 -0500

    exercise review completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,47 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.print("Heollo");
-        System.out.print("World");
+//        System.out.println("Hello, World!");
+//        System.out.print("Hello");
+//        System.out.print("World");
+        int myFavoriteNumber=33;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "My name is jaewoo baek";
+        System.out.println(myString);
+
+//        3. myString = 'a';
+//        4. myString = 3.14159
+//        System.out.println(myString);
+
+        float myNumber;
+
+        myNumber = (float)3.14F;
+
+//        myNumber = 123;
+
+        System.out.println(myNumber);
+        System.out.println();
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        System.out.println(three);
+
+        System.out.println(Integer.MAX_VALUE);
+
+        int myInt = 31;
+        Integer myInteger = 31;
+
+        double myDbl =42.42;
+        double myDouble=42.42;
+
+        System.out.println(myDbl);
+        System.out.println(myDouble);
 
     }
 }
