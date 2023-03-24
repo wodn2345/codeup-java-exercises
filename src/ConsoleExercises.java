bd230e8390cb9d87c0a922c9863047f7872fb787
Jaewoo Baek <jaewoo.baek01@gmail.com>
2023-03-24 17:02:06 -0500

    exercise for console

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,7 @@
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        int almostPi = (int) pi;
+        System.out.format("The value of pi is approximately %d", almostPi);
+    }
+}
