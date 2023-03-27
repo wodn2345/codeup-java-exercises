b177fff8af7246791031b6a91463b2cf3b21e824
Jaewoo Baek <jaewoo.baek01@gmail.com>
2023-03-27 13:55:44 -0500

    Control exercise going on

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,7 +1,6 @@
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        int almostPi = (int) pi;
-        System.out.format("The value of pi is approximately %d", almostPi);
+        System.out.format("The value of pi is approximately %d", pi);
     }
 }
---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,26 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        int i=5;
+//        while (i<15);
+//        System.out.println("i is" + i);
+//        i++;
+
+//        int i=0;
+//        do {
+//            System.out.println(i);
+//            i+=2;
+//        } while (i <=100);
+//
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+
+//        int num=2;
+//        do {
+//            System.out.println(num);
+//            num *= num;
+//        }while (num <1000000);
+    }
+}
