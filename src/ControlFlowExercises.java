a910ca2c51b66ef5acbf5c77c76f41dfc4f012e9
Jaewoo Baek <jaewoo.baek01@gmail.com>
2023-03-27 14:14:25 -0500

    control statements and loops exercise number 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -30,5 +30,17 @@ public class ControlFlowExercises {
 //        for(int i=2; i<100; i+=2) {
 //            System.out.println("i is " + i);
 //        }
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
