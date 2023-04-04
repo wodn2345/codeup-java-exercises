1e3adff3b09e39123da09f95d0c488f4ee075cdd
Jaewoo Baek <jaewoo.baek01@gmail.com>
2023-04-04 15:12:10 -0500

    exercises working on

--- src/util/Input.java
+++ src/util/Input.java
@@ -9,7 +9,8 @@ public class Input {
     }
 
     String getString(){
-        return scanner.nextLine();
+        System.out.println("Enter a string.");
+        return this.scanner.nextLine();
     }
     boolean yesNo() {
         String userInput=this.scanner.next();
@@ -18,16 +19,26 @@ public class Input {
     }
 
     int getInt(){
-
+        return this.scanner.nextInt();
     }
     int getInt(int min, int max){
-
-    }
-
-    double getDouble(double min, double max){
-
+        int userInt;
+        do {
+        System.out.printf("Please enter a whole number between %d and %d: %n", min, max);
+        userInt = this.scanner.nextInt();
+        } while(userInt > max || userInt < min);
+        return userInt;
     }
     double getDouble(){
-
+        return this.scanner.nextDouble();
     }
+    double getDouble(double min, double max){
+        Double userDouble;
+        do {
+            System.out.printf("Please enter a decimal number between %f: %n", min, max);
+            userDouble = this.scanner.nextDouble();
+        } while (userDouble> max || userDouble < min);
+        return userDouble;
+    }
+
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,30 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input input= new Input();
+
+//        String myString = input.getString();
+//
+//        System.out.println("myString = " +myString);
+//
+//        System.out.println("Do you like pizza?");
+//        boolean likePizza = input.yesNo();
+//        System.out.println("likePizza = " + likePizza);
+//
+//        System.out.println("Please enter a whole Number.");
+//        int myInt = input.getInt();
+//        System.out.println("myInt = " + myInt);
+//
+//        int myInt2 = input.getInt(1, 100);
+//        System.out.println("myInt2 = " + myInt2);
+//
+//        System.out.println("Please enter a decimal number.");
+//        double myDouble = input.getDouble();
+//        System.out.println("myDouble = " + myDouble);
+//
+//        double myDouble2= input.getDouble(1,100);
+//        System.out.println("myDouble2 = "+ myDouble2);
+
+    }
+}
